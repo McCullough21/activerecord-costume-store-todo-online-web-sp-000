46623d079a23f92b72f727de84a27eb3343191fb
Michael McCullough <michael.mccullough21@gmail.com>
2019-09-10 18:36:30 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costume.rb
+++ db/migrate/001_create_costume.rb
@@ -1,7 +1,7 @@
 class CreateCostumes < ActiveRecord::Migration[5.1]
 
   def change
-    create_table :costumes do |c|
+    create_table :costume do |c|
       c.string :name
       c.float :price
       c.string :size
